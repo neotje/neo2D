127194d4fa07bf2503ddd1940eb11511b4209857
neotje <neotje111@gmail.com>
2019-07-15 12:15:11 +0200

    update

---
+++ neo2D.js
@@ -0,0 +1,16 @@
+'use strict';
+
+if ($) {
+  console.log("jquery is loaded");
+} else {
+  console.log("jquery not loaded");
+}
+
+class neo2D {
+  constructor(canvas) {
+    var canvas = document.getElementById(canvas);
+    var context = canvas.getContext("2d");
+
+    var entities = [];
+  }
+}
